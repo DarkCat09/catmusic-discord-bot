63ab1e0ec2abb4ca4748a2707ab358d5eb7e65fb
Andrey <50486086+DarkCat09@users.noreply.github.com>
2021-07-06 17:46:45 +0400

    Switched to YouTube (instead of Yandex)

--- catmusicbot.py
+++ catmusicbot.py
@@ -2,7 +2,8 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
-import yandex_music
+from youtubesearchpython import VideosSearch
+from pytube import YouTube
 
 # Add bot to a server:
 # https://discord.com/api/oauth2/authorize?client_id=861599847441104926&permissions=2184277249&scope=bot
@@ -10,8 +11,6 @@ import yandex_music
 # Options
 token = '*** YOUR TOKEN ***'
 bot = commands.Bot(command_prefix='/')
-# Setting up YandexMusic
-ymcl = yandex_music.Client.from_credentials('achtest22@yandex.ru', 'tEs#t22')
 
 # Command /catplay <search query>
 @bot.command(name='catplay', description='Plays the music in a voice channel')
@@ -29,19 +28,16 @@ async def catplay(ctx, *args):
 		song_query = ' '.join(args)
 		await ctx.send(':mag: **Searching...**')
 
-		song = ymcl.search(song_query, True, 'track').tracks.results[0]
-		song.download(str(song.id) + '.mp3', 'mp3')
-		artist = ''
-
-		# Joining artists into a string
-		for artist_obj in song.artists:
-			artist += artist_obj.name + ', '
-		# Removing a comma in the end of the string
-		artist = artist[:len(artist)-2]
+		# Find music on YouTube and download it
+		vs = VideosSearch(song_query, limit=1)
+		yt = YouTube(vs.result()['result'][0]['link'])
+		await ctx.send(':open_file_folder: **Downloading...**')
+		dl = yt.streams.filter(only_audio=True).first().download()
+		os.rename(yt.streams.filter(only_audio=True).first().default_filename, 'music.mp3')
 
 		# Beautiful notification
-		notif = discord.Embed(color=0xe3813e, title='Playing:', description=f'{artist} - {song.title}')
-		notif.set_image(url=song.cover_uri)
+		notif = discord.Embed(color=0xe3813e, title='Playing:', description=f'{yt.author} - {yt.title}')
+		notif.set_image(url=yt.thumbnail_url)
 
 		# Connecting to the voice channel
 		sp = await music_channel.connect()
@@ -49,12 +45,12 @@ async def catplay(ctx, *args):
 		if (sp.is_playing()):
 			sp.stop()
 		# Playing music to the voice channel
-		sp.play(discord.FFmpegPCMAudio(source=(str(song.id) + '.mp3')), after=None)
+		sp.play(discord.FFmpegPCMAudio(source='music.mp3'), after=None)
 		await ctx.send(embed=notif)
 		while (sp.is_playing()):
 			await asyncio.sleep(1)
 		sp.stop()
-		os.remove(str(song.id) + '.mp3')
+		os.remove('music.mp3')
 
 # Starting bot after setup
 bot.run(token)
